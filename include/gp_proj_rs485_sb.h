e1133531608f496597a800669ffc1625fd74b847
Andrew K. Walker <andrew@badger-technologies.com>
2017-05-23 17:24:53 -0400

    Added some sensor bus packets.
    These needed to be checked in so that the ST code would compile.

--- include/gp_proj_rs485_sb.h
+++ include/gp_proj_rs485_sb.h
@@ -3,20 +3,26 @@
 
 #include "generic_packet.h"
 
-/* Packets when you know only one thing is connected. */
-#define RS485_SET_ADDRESS
-#define RS485_QUERY_ADDRESS
-#define RS485_RESP_ADDRESS
+typedef struct {
+   float x;
+   float y;
+   float z;
+   float roll;
+   float pitch;
+   float yaw;
+} PoseIsh;
 
+/* Packets when you know only one thing is connected. */
+#define RS485_SET_ADDRESS    0x01
+#define RS485_QUERY_ADDRESS  0x02
+#define RS485_RESP_ADDRESS   0x03
 /* Packets when everyone is on the bus. All of these packets will have an
  * address as the first byte of the
  */
-#define RS485_INITIATE_READINGS
-
 /* Work need to be done on what the INFO is and how to configure it. */
 /* Cannot SET_SENSOR_INFO, it will be programmed into the sensor itself. */
-#define RS485_QUERY_SENSOR_INFO
-#define RS485_RESP_SENSOR_INFO
+#define RS485_QUERY_SENSOR_INFO 0x04
+#define RS485_RESP_SENSOR_INFO  0x05
 /* SENSOR_INFO Defines
    -Locations/Pose:
     1) Fixed number of predefined locations in a single byte
@@ -29,9 +35,9 @@
     1) A single micro out there could have multiple sensors attached.
 
 */
-#define RS485_INITIATE_READING
-#define RS485_QUERY_DATA
-#define RS485_RESP_DATA
+#define RS485_INITIATE_READING 0x06
+#define RS485_QUERY_DATA       0x07
+#define RS485_RESP_DATA        0x08
 
 /* Do we need to tell the slaves about timeout values, or other communication
  * specific things?
@@ -43,5 +49,18 @@
 #define RS485_ADDRESS_MASTER        0xFD
 #define RS485_ADDRESS_CONFIGURATION 0xFE
 #define RS485_ADDRESS_BROADCAST     0xFF
+
+
+/* Sensor Types */
+#define RS485_SB_TYPE_PROXIMITY_SONAR
+#define RS485_SB_TYPE_PROXIMITY_VCSEL
+#define RS485_SB_TYPE_PROXIMITY_CAPACITIVE
+
+
+uint8_t create_rs485_query_sensor_info(GenericPacket *packet, uint8_t address);
+uint8_t extract_rs485_query_sensor_info(GenericPacket *packet, uint8_t *address);
+
+uint8_t create_rs485_resp_sensor_info(GenericPacket *packet, uint8_t address, uint8_t sensor_type, PoseIsh p);
+uint8_t extract_rs485_resp_sensor_info(GenericPacket *packet, uint8_t *address, uint8_t *sensor_type, PoseIsh *p);
 
 #endif
--- src/gp_proj_rs485_sb.c
+++ src/gp_proj_rs485_sb.c
@@ -1,2 +1,51 @@
 #include "gp_proj_rs485_sb.h"
 
+uint8_t create_rs485_query_sensor_info(GenericPacket *packet, uint8_t address)
+{
+   gp_reset_packet(packet);
+   gp_add_proj(packet, GP_PROJ_RS485_SB, RS485_QUERY_SENSOR_INFO);
+   gp_add_uint8(packet, address);
+   gp_add_checksum(packet);
+
+   return GP_SUCCESS;
+}
+
+uint8_t extract_rs485_query_sensor_info(GenericPacket *packet, uint8_t *address)
+{
+   gp_set_data_index(packet, 0);
+   gp_get_uint8(packet, address);
+
+   return GP_SUCCESS;
+}
+
+
+uint8_t create_rs485_resp_sensor_info(GenericPacket *packet, uint8_t address, uint8_t sensor_type, PoseIsh p)
+{
+   gp_reset_packet(packet);
+   gp_add_proj(packet, GP_PROJ_RS485_SB, RS485_RESP_SENSOR_INFO);
+   gp_add_uint8(packet, address);
+   gp_add_float32(packet, p.x);
+   gp_add_float32(packet, p.y);
+   gp_add_float32(packet, p.z);
+   gp_add_float32(packet, p.roll);
+   gp_add_float32(packet, p.pitch);
+   gp_add_float32(packet, p.yaw);
+   gp_add_checksum(packet);
+
+   return GP_SUCCESS;
+}
+
+
+uint8_t extract_rs485_resp_sensor_info(GenericPacket *packet, uint8_t *address, uint8_t *sensor_type, PoseIsh *p)
+{
+   gp_set_data_index(packet, 0);
+   gp_get_uint8(packet, address);
+   gp_get_float32(packet, &(p->x));
+   gp_get_float32(packet, &(p->y));
+   gp_get_float32(packet, &(p->z));
+   gp_get_float32(packet, &(p->roll));
+   gp_get_float32(packet, &(p->pitch));
+   gp_get_float32(packet, &(p->yaw));
+
+   return GP_SUCCESS;
+}
